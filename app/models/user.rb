19ef45ff1bb07785454fc4171ade979a65b904e2
Yohan Bendavid <yohan.p002@gmail.com>
2019-02-25 15:45:45 +0100

    method and views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
+  has_many :team_players
   has_many :teams, through: :team_players
-  belongs_to :team_players
   has_many :inscriptions, through: :teams
   has_many :tournaments, through: :inscriptions
   # Include default devise modules. Others available are:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_140237) do
+ActiveRecord::Schema.define(version: 2019_02_25_144233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_140237) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|

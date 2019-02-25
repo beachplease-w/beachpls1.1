1005314d98d6b99d8d7233c27dfbc38e4129a1a8
Yohan Bendavid <yohan.p002@gmail.com>
2019-02-25 15:28:27 +0100

    controller and views for tournaments created

---
+++ app/controllers/tournaments_controller.rb
@@ -0,0 +1,10 @@
+class TournamentsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @tournaments = Tournament.all
+  end
+
+  def show
+  end
+end

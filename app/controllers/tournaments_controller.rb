067fbd033e82264091f554579d6d136bc1bf5cae
Jules Rivoire <julesriv@gmail.com>
2019-02-25 18:15:52 +0100

    added tittle and icon to tab

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,5 +1,5 @@
 class TournamentsController < ApplicationController
-  # skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @tournaments = Tournament.all

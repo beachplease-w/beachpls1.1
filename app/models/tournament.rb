740ee40b7d09b0547d4e9d3980dd497e62982a6e
Marine Sourin <marinesourin@live.fr>
2019-02-26 17:31:15 +0100

    Address fix in Tournament model

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,7 +3,7 @@ class Tournament < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
   include PgSearch
   pg_search_scope :search_by_address_serie_and_name,
-            against: [:adress, :serie, :name],
+            against: [:address, :serie, :name],
             using: {
               tsearch: { prefix: true } # <-- now `superman batm` will return something!
             }

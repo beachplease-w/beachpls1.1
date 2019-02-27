9971fc3f6a23e56932c5f055037a995181d003b9
Marine Sourin <marinesourin@live.fr>
2019-02-27 14:42:44 +0100

    Dashboard started

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_27_094646) do
+ActiveRecord::Schema.define(version: 2019_02_27_113010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_094646) do
     t.string "last_name"
     t.integer "license_no"
     t.date "birth_date"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

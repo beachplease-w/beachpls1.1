c4033b9626c643e3dd5dfa24d486a96c5a223bcf
Jules Rivoire <julesriv@gmail.com>
2019-02-27 15:19:47 +0100

    created migration to add columns to tournament

---
+++ db/migrate/20190227140133_add_columns_to_tournament.rb
@@ -0,0 +1,22 @@
+class AddColumnsToTournament < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tournaments, :remarques, :text
+    add_column :tournaments, :equipe_tableau_principal, :integer
+    add_column :tournaments, :wildcard_principal, :integer
+    add_column :tournaments, :place_reservees_qualif, :integer
+    add_column :tournaments, :equipe_qualif, :integer
+    add_column :tournaments, :wildcard_qualif, :integer
+    add_column :tournaments, :terrains_normes, :integer
+    add_column :tournaments, :date_qualif, :date
+    add_column :tournaments, :date_principal, :date
+    add_column :tournaments, :prize_money, :integer
+    add_column :tournaments, :split_prize_money, :string
+    add_column :tournaments, :limit_inscription, :date
+    add_column :tournaments, :reunion_qualif, :datetime
+    add_column :tournaments, :reunion_principal, :datetime
+    add_column :tournaments, :ville, :string, null: false
+    add_column :tournaments, :club_organisateur, :string
+    add_column :tournaments, :contact, :string
+    add_column :tournaments, :genre, :boolean, null: false
+  end
+end

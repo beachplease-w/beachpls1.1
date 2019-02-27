6bceef775debf49781275f4e73664c9ea5e35b77
Marine Sourin <marinesourin@live.fr>
2019-02-27 10:47:10 +0100

    Add Photo to Tournaments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_093741) do
+ActiveRecord::Schema.define(version: 2019_02_27_094646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_093741) do
     t.string "name"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|

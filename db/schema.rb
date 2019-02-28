3a9c1a62700edaea8034635cb57e8253efe2a7ee
Jules Rivoire <julesriv@gmail.com>
2019-02-28 10:28:37 +0100

    jue

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_02_27_113010) do
-
+ActiveRecord::Schema.define(version: 2019_02_27_143515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +48,6 @@ ActiveRecord::Schema.define(version: 2019_02_27_113010) do
     t.string "name"
     t.float "latitude"
     t.float "longitude"
-    t.string "photo"
     t.text "remarques"
     t.integer "equipe_tableau_principal"
     t.integer "wildcard_principal"
@@ -71,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_02_27_113010) do
     t.boolean "genre"
     t.string "sku"
     t.integer "price_cents", default: 0, null: false
+    t.string "photo"
   end
 
   create_table "users", force: :cascade do |t|

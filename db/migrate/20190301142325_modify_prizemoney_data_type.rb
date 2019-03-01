b4669f593d2079c4f13225d7bf46bd1a8aa37fc6
Jules Rivoire <julesriv@gmail.com>
2019-03-01 15:49:55 +0100

    added the prices to the right places changed the genre logo, tournment inscription responsive on card

--- db/migrate/20190301142325_modify_prizemoney_data_type.rb
+++
@@ -1,6 +0,0 @@
-class ModifyPrizemoneyDataType < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tournaments, :prize_money
-    add_monetize :tournaments, :prize_money, currency: { present: false }
-  end
-end

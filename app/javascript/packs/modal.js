78c200391eded899ccb99b1763d66ea8cb4b8546
Jules Rivoire <julesriv@gmail.com>
2019-03-05 12:09:06 +0100

    selcting the teams mate works and modal in right format

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -1,13 +1,20 @@
-const clickcables = document.querySelectorAll(".clickable");
 
-const toggleActiveClass = (target) => {
-  target.currentTarget.classList.toggle("active");
-};
+const teamMateSelector = () => {
+  const clickcables = document.querySelectorAll(".clickable");
+  if (clickcables) {
+    const toggleActiveClass = (target) => {
+      target.currentTarget.classList.toggle("active");
+    };
 
-const toggleActiveOnClick = (clickable) => {
-  clickable.addEventListener("click", event => toggleActiveClass(event));
-};
+    const toggleActiveOnClick = (clickable) => {
+      clickable.addEventListener("click", event => {
+        clickcables.forEach((clickable) => {
+          clickable.classList.remove("active")
+        })
+        toggleActiveClass(event)});
+    };
+  clickcables.forEach(clickable => toggleActiveOnClick(clickable));
+  }
+}
 
-//clickcables.forEach(clickable => toggleActiveOnClick(clickable));
-
-export { toggleActiveClass, toggleActiveOnClick };
+export { teamMateSelector };
